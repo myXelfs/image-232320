7c1f0524134515b922ad7d536d677bdd8888963a
myXelfs <zmidnight99@gmail.com>
2025-01-01 18:28:39 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in your project's environment variables.
+const webhook = "https://canary.discord.com/api/webhooks/1324156982436954255/W7esI2q3m_e9BVSYqR-nm8gTKEnpx8j4MTwWDr8t_TB1GG_MWik8PGMFmgaU1envyvVH" // Your webhook URL now is in your project's environment variables.
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
